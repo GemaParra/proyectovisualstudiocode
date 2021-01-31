d018b33b46c8fda020bffffc7af01a3e9195bbd4
gema <gemaparra01@gmail.com>
2021-01-31 10:19:39 +0100

    Añadimos una línea

---
+++ parimpar.java
@@ -0,0 +1,32 @@
+import java.util.*;
+/**
+ *
+ * @author Gema
+ */
+public class parimpar {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+            Scanner sc = new Scanner (System.in);
+            System.out.print("Introduzca un número: ");
+            int x=sc.nextInt();
+
+            System.out.print("Introduzca un número: ");
+            int y=sc.nextInt();
+
+
+            if(y%x==0) {
+                    System.out.print("El número es múltiplo.");
+            } else {
+                    System.out.print("El número no es múltiplo.");
+            }
+            //fin del programa
+            System.out.print("\nFin del programa");
+            System.out.print("\nHasta la proxima :)");
+            
+    }
+    
+}
